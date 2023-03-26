2ff2611909a5b30b4f65a08bab873c7196dc280e
Mohit8820 <mohitprasadsah8820@gmail.com>
2023-03-26 10:40:52 +0530

    app with upper space

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState, useCallback } from "react";
+import {
+  StyleSheet,
+  Alert,
+  View,
+  BackHandler,
+  ActivityIndicator,
+} from "react-native";
+import { WebView } from "react-native-webview";
 
-export default function App() {
+const Loading = () => (
+  <ActivityIndicator
+    color="#72b34863"
+    size="large"
+    style={[styles.loading, { transform: [{ translateX: -10 }] }]}
+  />
+);
+
+export default function App({}) {
+  //   // this javascript will be injected on page load
+  //   const injectedJs = `
+  // window.postMessage(window.location.href);
+  // `;
+
+  const webviewref = useRef(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+  // const [canGoForward, setCanGoForward] = useState(false);
+  // const [currentUrl, setCurrentUrl] = useState(
+  //   "https://mohit8820.github.io/mohit.sah/"
+  // );
+
+  const backAction = useCallback(() => {
+    if (canGoBack && webviewref.current) {
+      webviewref.current.goBack();
+      return true;
+    }
+    return false;
+  }, [canGoBack]);
+
+  useEffect(() => {
+    BackHandler.addEventListener("hardwareBackPress", backAction);
+    return () =>
+      BackHandler.removeEventListener("hardwareBackPress", backAction);
+  }, [backAction]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}></View>
+      <WebView
+        ref={webviewref}
+        source={{ uri: "https://queryex.netlify.app/home" }}
+        onLoadProgress={(event) => setCanGoBack(event.nativeEvent.canGoBack)}
+        startInLoadingState
+        renderLoading={Loading}
+        /* source={{ uri: currentUrl }}
+        injectedJavaScript={injectedJs}
+        onNavigationStateChange={(navState) => {
+          console.log(navState);
+          setCanGoBack(navState.canGoBack);
+          setCanGoForward(navState.canGoForward);
+          setCurrentUrl(navState.url);
+        }}
+        onMessage={(event) => {
+          alert("MESSAGE >>>>" + event.nativeEvent.data);
+        }}
+        // onLoadStart={() => {
+        //   console.log("LOAD START ");
+        // }}
+        // onLoadEnd={() => {
+        //   console.log("LOAD END");
+        // }}
+        // onError={(err) => {
+        //   console.log("ERROR ");
+        //   console.log(err);
+        // }}*/
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: 25,
+    backgroundColor: "#f8f9f9",
+  },
+  loading: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
   },
 });
